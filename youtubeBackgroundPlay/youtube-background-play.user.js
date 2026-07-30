cd5a9b02b711b1137ba43db72cdfd92945d7cc26
João Pedro Veiga <jpveigao10@gmail.com>
2026-07-29 21:38:28 -0300

    Add YouTube Background Play userscript

---
+++ youtubeBackgroundPlay/youtube-background-play.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         YouTube Background Play (Firefox fix port)
+// @namespace    https://vgpnk-holdings-llc.github.io/
+// @version      1.0.0
+// @description  Keeps YouTube (and YouTube Music) playing when the tab is hidden or the screen is locked. Port of the "YouTube Background Playback" Firefox fix for userscript managers (Dguard).
+// @author       VeigaPunk
+// @match        *://*.youtube.com/*
+// @match        *://*.youtube-nocookie.com/*
+// @match        *://music.youtube.com/*
+// @grant        none
+// @run-at       document-start
+// @inject-into  page
+// @license      MIT
+// @downloadURL  https://github.com/AdguardTeam/Userscripts/raw/master/youtubeBackgroundPlay/youtube-background-play.user.js
+// @updateURL    https://github.com/AdguardTeam/Userscripts/raw/master/youtubeBackgroundPlay/youtube-background-play.user.js
+// ==/UserScript==
+
+CONTENT_PLACEHOLDER
